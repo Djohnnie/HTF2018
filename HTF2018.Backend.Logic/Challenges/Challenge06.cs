ab9301e9c6a223cd79e2da24229d5769f26e79a4
Seppe <van.winkel.seppe@me.com>
2018-11-21 22:23:56 +0100

    :recycle: Move challenges around to make place for a few easier ones in the front

--- HTF2018.Backend.Logic/Challenges/Challenge06.cs
+++ HTF2018.Backend.Logic/Challenges/Challenge06.cs
@@ -4,79 +4,52 @@ using HTF2018.Backend.Common.Model;
 using HTF2018.Backend.Logic.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace HTF2018.Backend.Logic.Challenges
 {
     public class Challenge06 : ChallengeBase, IChallenge06
     {
-        /// <summary>
-        /// CHALLENGE 06:
-        ///   The artifact is sending us messages again. See if you can decode them! This time they are sending a number with it, migt it be a key to decipher it?
-        /// </summary>
         public Challenge06(IHtfContext htfContext, ITeamLogic teamLogic, IChallengeLogic challengeLogic, IDashboardLogic dashboardLogic, IHistoryLogic historyLogic)
             : base(htfContext, teamLogic, challengeLogic, dashboardLogic, historyLogic) { }
 
-        private const string Cipher = "cipher";
-        private readonly Random _randomGenerator = new Random();
-        private readonly List<string> _artifactSentences = new List<string>
-        {
-            //To Change
-            "The artifact has landed on a sacred place.",
-            "We chose this location as the one with the biggest impact.",
-            "The humans are trying to decipher our language!",
-            "The artifact is getting breached, adapt!"
-        };
         public async Task<Challenge> GetChallenge()
         {
-            var challenge = await BuildChallenge(Identifier.Challenge06);
+            Challenge challenge = await BuildChallenge(Identifier.Challenge06);
             return challenge;
         }
 
         protected override Task<Question> BuildQuestion()
         {
-
-            var cipher = _randomGenerator.Next(1, 27);
             var question = new Question
             {
-                InputValues = new List<Value>
-                {
-                    new Value { Name = "encoded", Data = Encode(_artifactSentences[_randomGenerator.Next(_artifactSentences.Count)],cipher)},
-                    new Value { Name = Cipher, Data = $"{cipher}"}
-                }
+                InputValues = new List<Value>()
             };
+
+            // TODO: Add name-data pairs to the InputValues!
 
             return Task.FromResult(question);
         }
 
         protected override Task<Answer> BuildAnswer(Question question, Guid challengeId)
         {
-            var answers = new List<Value>();
+            // TODO: Calculate answer based on question!
 
-            var cipherValue = question.InputValues.Find(e => e.Name.Equals(Cipher));
-            var cipher = Convert.ToInt32(cipherValue.Data);
-            foreach (var inputValue in question.InputValues)
-            {
-                answers.Add(
-                    new Value { Name = "decoded", Data = Encode(inputValue.Data, -cipher) });
-            }
             return Task.FromResult(new Answer
             {
                 ChallengeId = challengeId,
-                Values = answers
+                Values = new List<Value>
+                {
+                    // TODO: Add name-data pairs containing answers!
+                }
             });
         }
 
         protected override async Task<Example> BuildExample(Guid challengeId)
         {
-            var question = new Question
+            Question question = new Question
             {
-                InputValues = new List<Value> {
-                    new Value{Name = Cipher, Data = "16"},
-                    new Value{Name = "encoded", Data = Encode("Artifact",16)},
-                    new Value{Name = "encoded", Data = Encode("Aliens",16)}
-                }
+                // TODO: Add name-data pairs containing an example question based on the actual question!
             };
 
             return new Example
@@ -88,38 +61,15 @@ namespace HTF2018.Backend.Logic.Challenges
 
         protected override void ValidateAnswer(Answer answer)
         {
-            var invalid = answer.Values == null;
-            if (answer.Values != null) { invalid = true; }
-            if (!answer.Values.Any(x => x.Name == "decoded")) { invalid = true; }
-            foreach (var answerValue in answer.Values.Where(x => x.Name.Equals("decoded")))
-            {
-                if (string.IsNullOrEmpty(answerValue.Data))
-                    invalid = true;
-            }
+            Boolean invalid = false;
+
+            // TODO: Do a basic validation of the answer object!
+            // (Null-checks, are properties correct, but no actual functional checks)
+
             if (invalid)
             {
                 throw new InvalidAnswerException();
             }
-        }
-
-        private static string Encode(string value, int shift)
-        {
-            var buffer = value.ToCharArray();
-            for (var i = 0; i < buffer.Length; i++)
-            {
-                char letter = buffer[i];
-                letter = (char)(letter + shift);
-                if (letter > 'z')
-                {
-                    letter = (char)(letter - 26);
-                }
-                else if (letter < 'a')
-                {
-                    letter = (char)(letter + 26);
-                }
-                buffer[i] = letter;
-            }
-            return new string(buffer);
         }
     }
 }
--- HTF2018.Backend.Logic/Challenges/Challenges.cs
+++ HTF2018.Backend.Logic/Challenges/Challenges.cs
@@ -6,21 +6,21 @@ namespace HTF2018.Backend.Logic.Challenges
 {
     public static class Challenges
     {
-        public static readonly Dictionary<Identifier, String> Titles = new Dictionary<Identifier, String>
+        public static readonly Dictionary<Identifier, string> Titles = new Dictionary<Identifier, String>
         {
             { Identifier.Challenge01, "Identify yourself!" },
             { Identifier.Challenge02, "A simple challenge to get started!" },
             { Identifier.Challenge03, "Let's get it more difficult!" },
             { Identifier.Challenge04, "Cipherific!" },
             { Identifier.Challenge05, "Age, Time, ..?" },
-            { Identifier.Challenge06, "Ciphers again?" },
+            { Identifier.Challenge06, "" },
             { Identifier.Challenge07, "" },
             { Identifier.Challenge08, "" },
             { Identifier.Challenge09, "" },
             { Identifier.Challenge10, "What is this image?" },
-            { Identifier.Challenge11, "" },
-            { Identifier.Challenge12, "" },
-            { Identifier.Challenge13, "" },
+            { Identifier.Challenge11, "Ciphers, again?" },
+            { Identifier.Challenge12, "Why would they send squares?" },
+            { Identifier.Challenge13, "Geo(de)coding" },
             { Identifier.Challenge14, "" },
             { Identifier.Challenge15, "" },
             { Identifier.Challenge16, "" },
@@ -30,21 +30,21 @@ namespace HTF2018.Backend.Logic.Challenges
             { Identifier.Challenge20, "" }
         };
 
-        public static readonly Dictionary<Identifier, String> Descriptions = new Dictionary<Identifier, String>
+        public static readonly Dictionary<Identifier, string> Descriptions = new Dictionary<Identifier, String>
         {
             { Identifier.Challenge01, "Our team of physicists investigated the artifact and found that it contains twenty secrets that need to be unlocked. These secrets are translated into challenges for you to work on. The first challenge is simply initializing the artifact by identifying yourself with our engineer-friendly-middleware-interface-system." },
             { Identifier.Challenge02, "It seems our basic intelligence is being tested and a simple sum of random integers is what is expected." },
             { Identifier.Challenge03, "It might have been a long time, but the artefact seems to have all the knowledge. Please answer it!" },
             { Identifier.Challenge04, "The artifact is sending us messages. See if you can decode them!" },
             { Identifier.Challenge05, "The artifact is trying sending us some sort of time, see if you can convert it on different planets!" },
-            { Identifier.Challenge06, "They are sending messages again. I can't seem to decode them, can you try again?" },
+            { Identifier.Challenge06, "" },
             { Identifier.Challenge07, "" },
             { Identifier.Challenge08, "" },
             { Identifier.Challenge09, "" },
             { Identifier.Challenge10, "The artifact is trying to communicate using modern methods and generates weird black and white images. What do these images represent and what is the underlying message?" },
-            { Identifier.Challenge11, "" },
-            { Identifier.Challenge12, "" },
-            { Identifier.Challenge13, "" },
+            { Identifier.Challenge11, "They are sending messages again. I can't seem to decode them, can you try again?" },
+            { Identifier.Challenge12, "Try to count the squares, beware it can get more difficult!" },
+            { Identifier.Challenge13, "Are these just numbers or do they have a different meaning.." },
             { Identifier.Challenge14, "" },
             { Identifier.Challenge15, "" },
             { Identifier.Challenge16, "" },
