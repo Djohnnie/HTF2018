aeb7d0eefbf4eebe65bfe6cb24c5c1cf72016d09
Seppe <van.winkel.seppe@me.com>
2018-11-23 19:17:02 +0100

    :sparkles: Everyone loves pokemon

--- HTF2018.Backend.Logic/Challenges/Challenge18.cs
+++ HTF2018.Backend.Logic/Challenges/Challenge18.cs
@@ -4,18 +4,38 @@ using HTF2018.Backend.Common.Model;
 using HTF2018.Backend.Logic.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Serializers;
 
 namespace HTF2018.Backend.Logic.Challenges
 {
+    /// <summary>
+    /// CHALLENGE 13:
+    ///   Pokemon
+    /// </summary>
     public class Challenge18 : ChallengeBase, IChallenge18
     {
-        public Challenge18(IHtfContext htfContext, ITeamLogic teamLogic, IChallengeLogic challengeLogic, IDashboardLogic dashboardLogic, IHistoryLogic historyLogic)
-            : base(htfContext, teamLogic, challengeLogic, dashboardLogic, historyLogic) { }
+        public Challenge18(IHtfContext htfContext, ITeamLogic teamLogic, IChallengeLogic challengeLogic,
+            IDashboardLogic dashboardLogic, IHistoryLogic historyLogic)
+            : base(htfContext, teamLogic, challengeLogic, dashboardLogic, historyLogic)
+        {
+            _client = new RestClient("https://pokeapi.co/api/v2/ ");
+        }
+
+        private readonly RestClient _client;
+        private readonly Random _randomGenerator = new Random();
+
+        private readonly List<Coordinate> _artifactLocations = new List<Coordinate>
+        {
+            //todo: ADD COORDINATES
+            new Coordinate {Latitude = "51.168716", Longitude = "4.432200",}
+        };
 
         public async Task<Challenge> GetChallenge()
         {
-            Challenge challenge = await BuildChallenge(Identifier.Challenge18);
+            var challenge = await BuildChallenge(Identifier.Challenge18);
             return challenge;
         }
 
@@ -23,33 +43,55 @@ namespace HTF2018.Backend.Logic.Challenges
         {
             var question = new Question
             {
-                InputValues = new List<Value>()
+                InputValues = new List<Value>
+                {
+                    new Value
+                    {
+                        Name = "#",
+                        Data = $"{_randomGenerator.Next(1, 802)}"
+                    }
+                }
             };
-
-            // TODO: Add name-data pairs to the InputValues!
 
             return Task.FromResult(question);
         }
 
         protected override Task<Answer> BuildAnswer(Question question, Guid challengeId)
         {
-            // TODO: Calculate answer based on question!
+            var pokemonNr = question.InputValues.Single(e => e.Name.Equals("#")).Data;
+            var request = new RestRequest("pokemon/{pokemonNr}");
+            request.AddUrlSegment("pokemonNr", pokemonNr);
+
+            var response = _client.Execute<Pokemon>(request);
+
 
             return Task.FromResult(new Answer
             {
                 ChallengeId = challengeId,
                 Values = new List<Value>
                 {
-                    // TODO: Add name-data pairs containing answers!
+                    new Value
+                    {
+                        Name = "name",
+                        Data =
+                            response.Data.Name
+                    }
                 }
             });
         }
 
         protected override async Task<Example> BuildExample(Guid challengeId)
         {
-            Question question = new Question
+            var question = new Question
             {
-                // TODO: Add name-data pairs containing an example question based on the actual question!
+                InputValues = new List<Value>
+                {
+                    new Value
+                    {
+                        Name = "#",
+                        Data = $"1"
+                    },
+                }
             };
 
             return new Example
@@ -61,15 +103,30 @@ namespace HTF2018.Backend.Logic.Challenges
 
         protected override void ValidateAnswer(Answer answer)
         {
-            Boolean invalid = false;
+            if (answer.Values == null)
+            {
+                throw new InvalidAnswerException();
+            }
 
-            // TODO: Do a basic validation of the answer object!
-            // (Null-checks, are properties correct, but no actual functional checks)
+            if (answer.Values != null)
+            {
+                throw new InvalidAnswerException();
+            }
 
-            if (invalid)
+            if (!answer.Values.Any(x => x.Name == "#"))
+            {
+                throw new InvalidAnswerException();
+            }
+
+            if (string.IsNullOrEmpty(answer.Values.Single(x => x.Name == "#").Data))
             {
                 throw new InvalidAnswerException();
             }
         }
+    }
+
+    public class Pokemon
+    {
+        [SerializeAs(Name = "name")] public string Name { get; set; }
     }
 }
